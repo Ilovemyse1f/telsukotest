67b191a75b48798a7885303106d31ba3967537f0
Ilovemyse1f <97678604+Ilovemyse1f@users.noreply.github.com>
2022-07-09 05:59:45 +0530

    Add files via upload

--- Telsuko.java
+++ Telsuko.java
@@ -1,3 +1,4 @@
+import java.util.*;
 class Telsuko{
 public static void main(String[] args){
 for(int i=1;i<=5;i++){
@@ -7,16 +8,62 @@ System.out.print("* ");
 else
 System.out.print("  ");
 }
-System.out.print(" ");
+System.out.println();
 }
-}
+System.out.println();
 for(int i=1;i<=5;i++){
 for(int j=1;j<=5;j++){
-if(i==1 ||j==3 ||i==5)
+if(i==1 ||j==1 ||i==3 ||i==5)
 System.out.print("* ");
 else
 System.out.print("  ");
 }
 System.out.println();
 }
+for(int i=1;i<=5;i++){
+for(int j=1;j<=5;j++){
+if(j==1 || i==5)
+System.out.print("* ");
+else
+System.out.print("  ");
+}
+System.out.println();
+}		
+for(int i=1;i<=5;i++){
+for(int j=1;j<=5;j++){
+if(i==1 ||(j==1&&i<=3)||i==3 ||(j==5&&i>=5)||i==5)
+System.out.print("* ");
+else
+System.out.print("  ");
+}
+System.out.println();
+}
+for(int i=1;i<=6;i++){
+for(int j=1;j<=6;j++){
+if(i==1 ||(j==1&&i<=3)||i==3 ||(j==6 &&i>=3)||i==6)
+System.out.print("* ");
+else
+System.out.print("  ");
+}
+System.out.println();
+}
+for(int i=1;i<=5;i++){
+for(int j=1;j<=5;j++){
+if(j==1 ||i==5 ||j==5)
+System.out.print("* ");
+else
+System.out.print("  ");
+}
+System.out.println();
+}
+for(int i=1;i<=5;i++){
+for(int j=1;j<=5;j++){
+if(j==1 ||(i==3
+System.out.print("* ");
+else
+System.out.print("  ");
+}
+System.out.println();
+}
+
 }}
